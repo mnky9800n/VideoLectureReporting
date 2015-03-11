5a1b8a756a6698bc0ad495e3099aa84069ce063a
mnky9800n <johnm.aiken@gmail.com>
2015-03-11 19:14:29 -0400

    made clickstream a class

--- VideoLectureReporting/experimental/ClickstreamImport.py
+++ VideoLectureReporting/experimental/ClickstreamImport.py
@@ -3,31 +3,37 @@
 import json
 import DatabaseConnection
 
-#print j.decoder.c_scanstring()
-
 'C:\Users\Administrator\Desktop\click data\phys1-001_clickstream_export'
 
-def get_clickstream_data(filename, value_str=False):
+class Clickstream:
+
+    """
+    This class provides various functionality to manipulate clickstream
+    data from the Coursera course platform. It is currently in
+    experimental version and does lots of nothing.
+    """
+
+    def get_clickstream_data(filename, value_str=False):
     
-    """
-    clickstream data comes in a json file and needs to be converted to
-    Python objects to upload to a SQL Database. 
+        """
+        clickstream data comes in a json file and needs to be converted to
+        Python objects to upload to a SQL Database. 
 
-    Returns a list of dictionaries.
+        Returns a list of dictionaries.
 
-    If value_str is True will return the value string as a dictionary
-    of Python objects too. This takes longer and uses a lot of memory.
-    """
+        If value_str is True will return the value string as a dictionary
+        of Python objects too. This takes longer and uses a lot of memory.
+        """
 
-    with open(filename, 'rb') as data:
+        with open(filename, 'rb') as data:
         
-        clickdata_raw = data.readlines()
+            clickdata_raw = data.readlines()
 
-        clickdata_decoded = map(json.loads, clickdata_raw)
+            clickdata_decoded = map(json.loads, clickdata_raw)
         
-        if value_str==True:
-            for c in clickdata_decoded:
-                c['value'] = json.loads(c['value'])
+            if value_str == True:
+                for c in clickdata_decoded:
+                    c['value'] = json.loads(c['value'])
 
-    return clickdata_decoded
+        return clickdata_decoded
 
